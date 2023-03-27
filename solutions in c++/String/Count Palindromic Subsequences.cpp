0e0cd7fb60fa42c4eed07a0581f7aed2d1acf69f
ronitkumar02 <107792345+ronitkumar02@users.noreply.github.com>
2023-03-27 16:34:37 +0000

    commit

---
+++ solutions in c++/String/Count Palindromic Subsequences.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+#define pii pair<int,int>
+#define vvi  vector<vector<int>>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
