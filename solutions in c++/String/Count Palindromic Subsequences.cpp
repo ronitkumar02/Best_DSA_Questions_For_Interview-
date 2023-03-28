987c7de210c4a0ebbb0ac966a4d4a57a64faf4a6
ronitkumar02 <107792345+ronitkumar02@users.noreply.github.com>
2023-03-28 05:56:36 +0000

    28/03/2023

--- solutions in c++/String/Count Palindromic Subsequences.cpp
+++ solutions in c++/String/Count Palindromic Subsequences.cpp
@@ -8,3 +8,30 @@ using namespace std;
 #define ll long long
 #define rep(i,a,b) for(int i=a;i<b;i++)
 
+int dp[1000][1000];
+string s;
+int n;
+
+int countPS(int i,int j){
+    if(i>j){
+        return 0;
+    }
+    if (dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    if(i==j){
+        return dp[i][j]=1;
+    }
+    else if(s[i]==s[j])
+    return dp[i][j]=countPS(i+1,j)+countPS(i,j-1)+1;
+    else
+    return dp[i][j]=countPS(i+1,j)+countPS(i,j-1)-countPS(i+1,j-1);
+}
+
+int main(){
+    memset(dp,-1,sizeof(dp));
+    cin>>s;
+    n=s.size();
+    cout<<countPS(0,n-1);
+    return 0;  
+}
